9d623d911bd1aec29cb2dec5735f866f28addd76
benfrank241 <benjamenba@yahoo.ca>
2022-07-05 16:35:07 -0400

    Working on header/home

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -13,7 +13,7 @@ class Header extends Component {
 
     return (
       <header id="home">
-        <ParticlesBg type="circle" bg={true} />
+        <ParticlesBg color="#ff0000" num={100} type="cobweb" bg={true} />
 
         <nav id="nav-wrap">
           <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
@@ -44,22 +44,17 @@ class Header extends Component {
 
             <li>
               <a className="smoothscroll" href="#portfolio">
-                Works
+                Projects
               </a>
             </li>
 
-            <li>
-              <a className="smoothscroll" href="#contact">
-                Contact
-              </a>
-            </li>
           </ul>
         </nav>
 
         <div className="row banner">
           <div className="banner-text">
             <Fade bottom>
-              <h1 className="responsive-headline">{name}</h1>
+            <h1 className="responsive-headline">{name}</h1>
             </Fade>
             <Fade bottom duration={1200}>
               <h3>{description}.</h3>
